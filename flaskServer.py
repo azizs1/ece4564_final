75afa17670ce6b3c70ea2be9698453be35343a90
azizs1 <azizs@vt.edu>
2021-12-07 03:18:18 -0500

    Basic form retrieval

--- flaskServer.py
+++ flaskServer.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 import socket
 from flask_httpauth import HTTPBasicAuth
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 import requests
 from zeroconf import ServiceBrowser, Zeroconf
 import threading
-from servicesKeys import *
+# from servicesKeys import *
 
 # Create a Flask object named app
 app = Flask(__name__)
@@ -30,64 +30,64 @@ LEDcolors = []
 
 # Define class for listening for zeroconf advertising
 # Starter code taken from Zeroconf Github page - https://github.com/jstasiak/python-zeroconf
-class Listener:
-    def remove_service(self, zeroconf, type, name):
-        print("Zeroconf: Service %s removed" % (name,))
+# class Listener:
+#     def remove_service(self, zeroconf, type, name):
+#         print("Zeroconf: Service %s removed" % (name,))
 
-    def update_service():
-        pass
+#     def update_service():
+#         pass
 
-    def add_service(self, zeroconf, type, name):
-        global LEDip
-        global LEDport
-        global LEDcolors
+#     def add_service(self, zeroconf, type, name):
+#         global LEDip
+#         global LEDport
+#         global LEDcolors
 
-        info = zeroconf.get_service_info(type, name)
+#         info = zeroconf.get_service_info(type, name)
 
-        if (info):
+#         if (info):
 
-            print("Zeroconf: Service %s added, service info: %s" % (name, info))
+#             print("Zeroconf: Service %s added, service info: %s" % (name, info))
 
-            # Grab IP address and port from ServiceInfo object
-            LEDip = socket.inet_ntoa(info.addresses[0])
-            print("Zeroconf: IP address found: " + LEDip)
-            LEDport = info.port
-            print("Zeroconf: Port found: " + str(LEDport))
+#             # Grab IP address and port from ServiceInfo object
+#             LEDip = socket.inet_ntoa(info.addresses[0])
+#             print("Zeroconf: IP address found: " + LEDip)
+#             LEDport = info.port
+#             print("Zeroconf: Port found: " + str(LEDport))
 
-            # Grab list of colors from ServiceInfo object
-            print("Zeroconf: Colors available: " + str(info.properties))
-            LEDcolors = info.properties[b'Colors']
+#             # Grab list of colors from ServiceInfo object
+#             print("Zeroconf: Colors available: " + str(info.properties))
+#             LEDcolors = info.properties[b'Colors']
         
-        else:
-            print("Zeroconf: No service found")
+#         else:
+#             print("Zeroconf: No service found")
 
             
-# Run zeroconf listener to check for new services
-def runZeroconf():
-    zc = Zeroconf()
-    listener = Listener()
-    browser = ServiceBrowser(zc, "_http._tcp.local.", listener)
-    print("Zeroconf: Initialized")
+# # Run zeroconf listener to check for new services
+# def runZeroconf():
+#     zc = Zeroconf()
+#     listener = Listener()
+#     browser = ServiceBrowser(zc, "_http._tcp.local.", listener)
+#     print("Zeroconf: Initialized")
 
-t = threading.Thread(target=runZeroconf)
-t.daemon = True
-t.start()
+# t = threading.Thread(target=runZeroconf)
+# t.daemon = True
+# t.start()
 
 # Callback function that verifies credentials by searching for them on the database
-@auth.verify_password
-def verify_password(username, password):
-    # Find an instance of this username and password pair in the database
-    user = col.find_one({'username': username, 'password': password})
+# @auth.verify_password
+# def verify_password(username, password):
+#     # Find an instance of this username and password pair in the database
+#     user = col.find_one({'username': username, 'password': password})
 
-    # If the user is found then return the username, otherwise return nothing
-    if user:
-        return username
-    return None
+#     # If the user is found then return the username, otherwise return nothing
+#     if user:
+#         return username
+#     return None
   
 # Error Handler function which gives message when login credentials fail
-@auth.error_handler
-def unauthorized():
-    return '401 - Unauthorized: Access is denied due to invalid credentials'
+# @auth.error_handler
+# def unauthorized():
+#     return '401 - Unauthorized: Access is denied due to invalid credentials'
   
 # Functions for API calls here
 # Stock stuff here
@@ -95,21 +95,28 @@ def unauthorized():
 
 
 # Post commands for the LED
-@app.route('/LED', methods=['GET','POST'])
-@auth.login_required
-def led_command():
+# @app.route('/LED', methods=['GET','POST'])
+# @auth.login_required
+# def led_command():
 
-    # Take in URL parameters
-    command = request.args.get('command')
+#     # Take in URL parameters
+#     command = request.args.get('command')
 
-    color = command
+#     color = command
 
-    # Send post request to LED
-    r = requests.post('http://'+ LEDip +':'+ str(LEDport) +'/LED?color='+ color)
+#     # Send post request to LED
+#     r = requests.post('http://'+ LEDip +':'+ str(LEDport) +'/LED?color='+ color)
 
-    return r.text
+#     return r.text
 
+@app.route('/login', methods=['POST'])
+def login_page():
+    message = ''
+    email = request.form.get('email')  # access the data inside 
+    password = request.form.get('password')
+    print(email,password)
+    return render_template("login.html", message=message)
 
 # Run the application when service is started
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
